e34035c4241e449e6b3bcd28a5601408ac783936
kaniikura <constantl492@gmail.com>
2020-07-12 12:29:17 +0900

    Update

--- abc/169/a.cpp
+++ abc/169/a.cpp
@@ -17,22 +17,3 @@ int main()
 
     solve();
 }
-#include <iostream>
-#include <string>
-using namespace std;
-
-int a, b;
-
-void solve()
-{
-    int ans;
-    ans = a * b;
-    cout << ans;
-}
-
-int main()
-{
-    cin >> a >> b;
-
-    solve();
-}
